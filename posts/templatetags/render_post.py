c89bcca061d50456db438dbb8aba189e3714511d
Edna Piranha <jen@ednapiranha.com>
2011-07-21 11:42:06 -0400

    readded audio

--- posts/templatetags/render_post.py
+++ posts/templatetags/render_post.py
@@ -26,10 +26,13 @@ def generate_post(value, post):
         if post.description.lower().find('vimeo') > -1:
             media = '<iframe src="http://player.vimeo.com/video/'+str(url.path.strip('/'))+'?wmode=transparent" width="70%" height="300"></iframe>'
         elif post.description.lower().find('youtube') > -1:
-            try:
-                media = '<iframe class="youtube-player" type="text/html" width="70%" height="300" src="http://youtube.com/embed/'+str(url.query.split('&')[0].split('v=')[1])+'"></iframe>'
-            except:
-                media = '<p>'+post.description+'</p>'
+            media = '<iframe class="youtube-player" type="text/html" width="70%" height="300" src="http://youtube.com/embed/'+str(url.query.split('v=')[1])+'"></iframe>'
+    elif isinstance(post, AudioPost):
+        if post.description.endswith('mp3'):
+            audio_type = 'audio/mpeg'
+        else:
+            audio_type = 'audio/ogg'
+        media = '<audio controls="controls" preload="auto"><source src="'+post.description+'" type="'+audio_type+'" /></audio><p><a href="'+post.description+'">'+post.description+'</a></p>'
     else:
         media = '<p>'+post.description+'</p>'
     return media
