3f6790e1bfff5c0095d3c997409a8411a68ea174
Edna Piranha <jen@ednapiranha.com>
2011-07-21 10:32:26 -0400

    testing fix for list index out of range

--- posts/templatetags/render_post.py
+++ posts/templatetags/render_post.py
@@ -26,7 +26,10 @@ def generate_post(value, post):
         if post.description.lower().find('vimeo') > -1:
             media = '<iframe src="http://player.vimeo.com/video/'+str(url.path.strip('/'))+'?wmode=transparent" width="70%" height="300"></iframe>'
         elif post.description.lower().find('youtube') > -1:
-            media = '<iframe class="youtube-player" type="text/html" width="70%" height="300" src="http://youtube.com/embed/'+str(url.query.split('&')[0].split('v=')[1])+'"></iframe>'
+            try:
+                media = '<iframe class="youtube-player" type="text/html" width="70%" height="300" src="http://youtube.com/embed/'+str(url.query.split('&')[0].split('v=')[1])+'"></iframe>'
+            except:
+                media = '<p>'+post.description+'</p>'
     else:
         media = '<p>'+post.description+'</p>'
     return media
